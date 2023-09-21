dc3f3397ad572e4eea4f20da8bd9c9075a4244c3
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-21 14:31:52 -0700

    adding-c-file-bub-pgm

---
+++ test/0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+/**
+ *bubble_sort - This function sort a given array
+ *@array: an array to be sorted
+ *@size: size of array to be sorted
+ * Return: doesn't return any value
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, j;
+	int swap;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = swap;
+			}
+		}
+	}
+}
