9221f25dc6f3405b08498b0060e49e9a9f739062
HoussineTabi <0620036304ht@gmail.com>
2023-09-22 07:41:27 +0000

    task2

--- test/2-selection_sort.c
+++ test/2-selection_sort.c
@@ -11,23 +11,22 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, min;
 	int swap;
 
-	if (size >= 2)
+	if (size == 0 || size == 1)
+		return;
+	for (i = 0; i < size - 1; i++)
 	{
-		for (i = 0; i < size - 1; i++)
+		min = i;
+		for (j = i; j <= size - 1; j++)
 		{
-			min = i;
-			for (j = i; j <= size - 1; j++)
-			{
-				if (array[j] < array[min])
-					min = j;
-			}
-			if (min != i)
-			{
-				swap = array[i];
-				array[i] = array[min];
-				array[min] = swap;
-			}
-			print_array(array, size);
+			if (array[j] < array[min])
+				min = j;
 		}
+		if (min != i)
+		{
+			swap = array[i];
+			array[i] = array[min];
+			array[min] = swap;
+		}
+		print_array(array, size);
 	}
 }
