8d0af75dc08b096072d0dca6d352c83721b77090
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-23 06:31:11 -0700

    some_changes

--- sort.h
+++ sort.h
@@ -5,7 +5,13 @@
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void selection_sort(int *, size_t);
-<<<<<<< HEAD
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 typedef struct listint_s
 {
 	const int n;
@@ -14,7 +20,6 @@ typedef struct listint_s
 } listint_t;
 void insertion_sort_list(listint_t **list);
 void swap_nodes(listint_t **list, listint_t **node1, listint_t *node2);
-=======
 void quick_sort(int *, size_t);
->>>>>>> refs/remotes/origin/master
+void print_list(const listint_t *);
 #endif /* SORT_H */
--- test/sort.h
+++ test/sort.h
@@ -5,7 +5,6 @@
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void selection_sort(int *, size_t);
-<<<<<<< HEAD
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,13 +14,12 @@ void selection_sort(int *, size_t);
  */
 typedef struct listint_s
 {
-const int n;
-struct listint_s *prev;
-struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
-void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
-=======
+void swap_nodes(listint_t **list, listint_t **node1, listint_t *node2);
 void quick_sort(int *, size_t);
->>>>>>> refs/remotes/origin/master
+void print_list(const listint_t *);
 #endif /* SORT_H */
